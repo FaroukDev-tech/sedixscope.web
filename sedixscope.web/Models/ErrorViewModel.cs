453771b1eebea916b9bac81490f6efeff8069cec
Far9999 <sadikalhanssah@gmail.com>
2024-05-26 12:55:24 +0000

    initialized project

---
+++ sedixscope.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace sedixscope.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
